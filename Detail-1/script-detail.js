cf3716d912dd85ee13bbd03925ff822131713b7a
Kaigagi <kaigagipro@gmail.com>
2021-06-23 10:38:16 +0700

    finally fix the bug

--- Detail-1/script-detail.js
+++ Detail-1/script-detail.js
@@ -1,38 +1,5 @@
-let book;
-let id;
-function getData(event) {
-     // console.log(1);
-    console.log(event.data);
-    book = event.data;
-    bookImage = document.getElementById("bookImage");
-    bookImage.src = event.data.image;
-    window.removeEventListener("message",getData);
-    id = book.id;
+function test(params) {
+    $.getJSON("../database.json",function(data) {
+        console.log(data.Books);
+    })
 }
-window.addEventListener("message",getData,false);
-
-// function getIDfromButton(event)
-// {
-//     let button = event.target;
-//     let purchasePage = window.open('../Purchase/purchase.html');
-//     setTimeout(function sendMessage() {
-//         console.log(button.id);
-//         purchasePage.postMessage(button.id,"http://127.0.0.1:5500");
-//     },3000);
-// }
-let number = document.getElementsByClassName("number");
-let amount = number[0].innerHTML
-function Increase() {
-    amount+=1;
-}
-
-function Decrease() {
-    
-}
-
-function putIntoStorage(book) {
-    console.log(1);
-    localStorage.setItem(id,book);
-    window.open('../Purchase/purchase.html');
-}
-
